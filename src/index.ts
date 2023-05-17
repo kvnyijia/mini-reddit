333f98123c06ca7741de6a99bc2c0427374f83cb
Yijia Chen <kevinc110604@gmail.com>
2023-05-17 19:15:39 -0400

    [new] Add db migration using MikroORM

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('hioewfmwoe')
+import { MikroORM } from "@mikro-orm/core"
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config"
+
+const main = async () => {
+  console.log('---------------------------------------------\n\n');
+  const orm = await MikroORM.init(microConfig);
+  // await orm.getMigrator().up();
+  // await orm.getMigrator().down();
+
+  // const post = orm.em.fork({}).create(Post, {title: "my first post"});
+  // await orm.em.fork({}).persistAndFlush(post);
+
+  const allposts = await orm.em.fork({}).find(Post, {});
+  console.log(allposts);
+};
+
+main().catch((err) => {
+  console.error(err);
+});  
