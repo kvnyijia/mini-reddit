fc4aaebcfe5b9039663becfc2c35fa28ef5aa409
Yijia Chen <kevinc110604@gmail.com>
2023-05-19 17:21:30 -0400

    [new] Add CRUD for posts

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,45 @@
-import { MikroORM } from "@mikro-orm/core"
+import "reflect-metadata";
+import { MikroORM } from "@mikro-orm/core";
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import { buildSchema } from "graphql";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config"
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+
 
 const main = async () => {
-  console.log('---------------------------------------------\n\n');
+  console.log('>>> ---------------------------------------------\n\n');
   const orm = await MikroORM.init(microConfig);
-  // await orm.getMigrator().up();
+  await orm.getMigrator().up();
   // await orm.getMigrator().down();
 
-  // const post = orm.em.fork({}).create(Post, {title: "my first post"});
+  // const post = orm.em.fork({}).create(Post, {title: "my 3rd post"});
   // await orm.em.fork({}).persistAndFlush(post);
 
-  const allposts = await orm.em.fork({}).find(Post, {});
-  console.log(allposts);
+  // const allposts = await orm.em.fork({}).find(Post, {});
+  // console.log(allposts);
+
+
+  const app = express();
+
+  // const apolloServer = new ApolloServer({
+  //   schema: await buildSchema({
+  //     resolvers: [HelloResolver, PostResolver],
+  //     validator: false,
+  //   }), 
+  //   context: () => ({ em: orm.em }),
+  // });
+  // apolloServer.applyMiddleware({ app });
+  app.get("/", (req, res) => {
+    res.send("hello");
+  });
+  app.listen(4000, () => {
+    console.log(">>> server started in localhost:4000");
+  });
+
 };
 
 main().catch((err) => {
---
+++ src/resolvers/post.ts
@@ -0,0 +1,57 @@
+import { Post } from "../entities/Post";
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
+import { MyContext } from "../types"
+
+@Resolver()
+export class PostResolver {
+  @Query(() => [Post]) 
+  posts(
+    @Ctx() ctx: MyContext
+  ): Promise<Post[]> {
+    return ctx.em.find(Post, {});
+  }
+
+  @Query(() => Post, {nullable: true}) 
+  post(
+    @Arg('id', () => Int) id: number,
+    @Ctx() ctx: MyContext
+  ): Promise<Post|null> {
+    return ctx.em.findOne(Post, {id});
+  }
+
+  @Mutation(() => Post) 
+  async createPost(
+    @Arg('title', () => String) title: string,
+    @Ctx() ctx: MyContext
+  ): Promise<Post> {
+    const post = ctx.em.create(Post, { title: title, createdAt: '', updatedAt: '' });
+    await ctx.em.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Post, {nullable: true}) 
+  async updatePost(
+    @Arg('id', () => Int) id: number,
+    @Arg('title', () => String, {nullable: true}) title: string,
+    @Ctx() ctx: MyContext
+  ): Promise<Post | null> {
+    const post = await ctx.em.findOne(Post, {id});
+    if (!post) {
+      return null;
+    }
+    if (typeof title !== "undefined") {
+      post.title = title;
+      await ctx.em.persistAndFlush(post);
+    }    
+    return post;
+  }
+
+  @Mutation(() => Boolean) 
+  async deletePost(
+    @Arg('id', () => Int) id: number,
+    @Ctx() ctx: MyContext
+  ): Promise<boolean> {
+    await ctx.em.nativeDelete(Post, { id });
+    return true;
+  }
+}
