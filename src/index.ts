b6b81ff7de978a151914638866fc634b90a0a3ff
Yijia Chen <kevinc110604@gmail.com>
2023-05-20 20:46:09 -0400

    [fix] Set up GraphQL sever using Apollo Server 4
    Migrating to Apollo Server 4:
    https://www.apollographql.com/docs/apollo-server/migration/#migrate-from-apollo-server-express

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
 import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
-import { ApolloServer } from "apollo-server-express";
+import { ApolloServer } from "@apollo/server";
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 import express from "express";
-import { buildSchema } from "graphql";
+import { expressMiddleware } from '@apollo/server/express4';
+import http from 'http';
+import cors from 'cors';
+import { json } from 'body-parser';
+import { typeDefs, resolvers } from './schema';
+import { MyContext } from "./types"
+// import { buildSchema } from "graphql";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config"
-import { HelloResolver } from "./resolvers/hello";
-import { PostResolver } from "./resolvers/post";
-
+// import { HelloResolver } from "./resolvers/hello";
+// import { PostResolver } from "./resolvers/post";
+// import { UserResolver } from "./resolvers/user";
 
 const main = async () => {
-  console.log('>>> ---------------------------------------------\n\n');
+  console.log('\n>>> ---------------------------------------------\n');
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
   // await orm.getMigrator().down();
@@ -24,21 +30,37 @@ const main = async () => {
 
 
   const app = express();
-
-  // const apolloServer = new ApolloServer({
+  const httpServer = http.createServer(app);
+  const apolloServer = new ApolloServer<MyContext>({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   //   schema: await buildSchema({
   //     resolvers: [HelloResolver, PostResolver],
   //     validator: false,
   //   }), 
   //   context: () => ({ em: orm.em }),
-  // });
+  });
+  await apolloServer.start();
+  app.use(
+    '/graphql',
+    cors<cors.CorsRequest>(),
+    json(),
+    expressMiddleware(apolloServer, {
+      context: async () => ({ em: orm.em }),
+    }),
+  );
+  
+  await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
+  console.log(`\n>>> Server ready at http://localhost:4000/graphql\n`);
+
   // apolloServer.applyMiddleware({ app });
-  app.get("/", (req, res) => {
-    res.send("hello");
-  });
-  app.listen(4000, () => {
-    console.log(">>> server started in localhost:4000");
-  });
+  // app.get("/", (req, res) => {
+  //   res.send("hello");
+  // });
+  // app.listen(4000, () => {
+  //   console.log(">>> server started in localhost:4000");
+  // });
 
 };
 
---
+++ src/schema.ts
@@ -0,0 +1,30 @@
+// import { Post } from "./entities/Post";
+
+// Schema definition
+export const typeDefs = `#graphql
+  type numberSix {
+    num: Int
+  }
+
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    sayHello: String
+    getNumberSix: numberSix
+  }
+`;
+
+// Resolver map
+export const resolvers = {
+  Query: {
+    sayHello() {
+      return "hello ;)";
+    },
+    getNumberSix() {
+      return {num: 6};
+    },
+  },
+};
