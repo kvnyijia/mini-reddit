9c2b72591fa43e4aac5001da7a39f841ca0b9da4
Yijia Chen <kevinc110604@gmail.com>
2023-05-22 01:19:49 -0400

    [new] Add connection to Redis & sending cookies
    But fail to handle the correct type of `req.session.userId`

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,11 @@ import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
 import { buildTypeDefsAndResolvers } from "type-graphql";
 
+// import RedisStore from "connect-redis";
+import session from "express-session";
+import { createClient } from "redis";
+// import connectRedis from 'connect-redis';
+
 const main = async () => {
   console.log('\n>>> ---------------------------------------------\n');
   const orm = await MikroORM.init(microConfig);
@@ -27,6 +32,38 @@ const main = async () => {
   });
 
   const app = express();
+
+  // Initialize client.
+  const redisClient = createClient();
+  // redisClient.on('error', err => console.log('Redis Client Error', err));
+  // await redisClient.connect();
+  redisClient.connect().catch(console.error);
+  // Initialize store.
+  // const RedisStore = connectRedis(session);
+  const RedisStore = require("connect-redis").default;
+  const redisStore = new RedisStore({
+    client: redisClient,
+    // prefix: "myapp:",
+    disableTouch: true,
+  });
+
+  // Initialize sesssion storage.
+  app.use(
+    session({
+      name: "myCookie",
+      store: redisStore,
+      resave: false, // required: force lightweight session keep alive (touch)
+      saveUninitialized: false, // recommended: only save session when data exists
+      secret: "keyboard cat",
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+        httpOnly: true,
+        sameSite: 'lax',
+        secure: __prod__,
+      },
+    })
+  );
+
   const httpServer = http.createServer(app);
   const apolloServer = new ApolloServer<MyContext>({
     typeDefs,
@@ -36,10 +73,10 @@ const main = async () => {
   await apolloServer.start();
   app.use(
     '/graphql',
-    cors<cors.CorsRequest>(),
+    cors<cors.CorsRequest>(),  // { origin: ["https://studio.apollographql.com"], credentials: true, }
     json(),
     expressMiddleware(apolloServer, {
-      context: async () => ({ em: orm.em }),
+      context: async ({ req, res }): Promise<MyContext> => ({ em: orm.em, req, res }),
     }),
   );
   
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,6 +1,6 @@
 import { User } from "../entities/User";
 import { MyContext } from "src/types";
-import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Resolver } from "type-graphql";
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Query, Resolver } from "type-graphql";
 import argon2 from "argon2";
 
 @InputType()
@@ -32,10 +32,22 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+  @Query(() => User, {nullable: true})
+  async me (
+    @Ctx() {req, em}: MyContext
+  ) {
+    if (!req.session.userId) {
+      return null;
+    }
+    const user = await em.findOne(User, {id: req.session.userId});
+    return user; 
+  }
+
+
   @Mutation(() => UserResponse) 
   async register(
     @Arg('options', () => UsernamePasswordInput) options: UsernamePasswordInput,
-    @Ctx() {em}: MyContext
+    @Ctx() {em, req}: MyContext
   ): Promise<UserResponse> {
     if (options.username.length <= 2) {
       return {
@@ -83,13 +95,14 @@ export class UserResolver {
       }
       console.log("message: ", err.message);
     }
+    req.session.userId = user.id;
     return {user};
   }
 
   @Mutation(() => UserResponse) 
   async login(
     @Arg('options', () => UsernamePasswordInput) options: UsernamePasswordInput,
-    @Ctx() {em}: MyContext
+    @Ctx() {em, req}: MyContext
   ): Promise<UserResponse> {
     const user = await em.findOne(User, {username: options.username});
     if (!user) {
@@ -109,7 +122,9 @@ export class UserResolver {
         }],
       };
     }
-    await em.persistAndFlush(user);
+    
+    req.session.userId = user.id;
+    // await em.persistAndFlush(user);
     return {user};
   }
 }
