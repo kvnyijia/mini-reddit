a5dc91edc10f774a0449f946926cbd0b28040e39
Yijia Chen <kevinc110604@gmail.com>
2023-06-04 21:27:54 -0400

    [refactor] Refactor src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -14,24 +14,19 @@ import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
 import { buildTypeDefsAndResolvers } from "type-graphql";
-
-// import RedisStore from "connect-redis";
 import session from "express-session";
-// import { createClient } from "redis";
 import Redis from "ioredis";
-// import { sendEmail } from "./utils/sendEmail";
-// import { User } from "./entities/User";
-// import connectRedis from 'connect-redis';
+const RedisStore = require("connect-redis").default;   // import RedisStore from "connect-redis";
 
 const main = async () => {
   console.log('\n>>> ---------------------------------------------\n');
-  // sendEmail("bob@bob.com", "hello this is email content");
+
+  // DB migration
   const orm = await MikroORM.init(microConfig);
-  // await orm.em.nativeDelete(User, {});
   await orm.getMigrator().up();
   // await orm.getMigrator().down();
 
-  // Generate the GraphQL schema: Create typeDefs and resolvers map k 
+  // Generate the GraphQL schema: Create a `typeDefs` and `resolvers`(map) pair
   const { typeDefs, resolvers } = await buildTypeDefsAndResolvers({
     resolvers: [HelloResolver, PostResolver, UserResolver],
   });
@@ -43,33 +38,22 @@ const main = async () => {
       credentials: true,
     })
   );
-  // app.set("Access-Control-Allow-Origin", "http://localhost:3000");
-  // app.set("Access-Control-Allow-Credentials", true);
-  // app.set('trust proxy', !__prod__);
 
-  // Initialize client.
-  // const redisClient = createClient();
-  // redisClient.on('error', err => console.log('Redis Client Error', err));
-  // await redisClient.connect();
-  // redisClient.connect().catch(console.error);
+  // Create redis (redis client) & redisStore
   const redis = new Redis();
-
-  // Initialize store.
-  // const RedisStore = connectRedis(session);
-  const RedisStore = require("connect-redis").default;
   const redisStore = new RedisStore({
     client: redis,
     // prefix: "myapp:",
     disableTouch: true,
   });
 
-  // Initialize sesssion storage.
+  // Initialize redis sesssion storage
   app.use(
     session({
       name: "myCookie",
       store: redisStore,
-      resave: false, // required: force lightweight session keep alive (touch)
-      saveUninitialized: false, // recommended: only save session when data exists
+      resave: false,                               // required: force lightweight session keep alive (touch)
+      saveUninitialized: false,                    // recommended: only save session when data exists
       secret: "keyboard cat",
       cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
@@ -96,15 +80,13 @@ const main = async () => {
     }),
   );
   
-  app.get("/", (_, res) => {
-    // res.send("hello");
-    // res.send(JSON.stringify({message: "hello world"}))
-    res.send(JSON.stringify(["apple", "banana", "canada"]))
-  });
+  // app.get("/", (_, res) => {
+  //   res.send(JSON.stringify({message: "hello world"}))
+  // });
   await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
   console.log(`\n>>> Server ready at http://localhost:4000/graphql\n`);
 };
 
 main().catch((err) => {
   console.error(err);
-});  
+});
