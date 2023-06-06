b63474558c523d25db3a4255cb1ebddcfa5c216e
Yijia Chen <kevinc110604@gmail.com>
2023-06-06 16:30:34 -0400

    [new] Add check login mech in /create-post

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
 export const __prod__ = process.env.NODE_ENV === 'production';
 export const FORGET_PASSWORD_PREFIX = 'forget-password:';
+export const NOT_LOGIN_ERROR_MSG = 'Not login';
--- src/middleware/isAuth.ts
+++ src/middleware/isAuth.ts
@@ -1,10 +1,11 @@
 import { MyContext } from "src/types";
 import { MiddlewareFn } from "type-graphql";
+import { NOT_LOGIN_ERROR_MSG } from "../constants";
 
 // A middleware is run before ur resolvers
 export const isAuth: MiddlewareFn<MyContext> = ({context}, next) => {
   if (!context.req.session.userId) {
-    throw new Error("Not login");
+    throw new Error(NOT_LOGIN_ERROR_MSG);
   }
   return next();
 };
