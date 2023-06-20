2607679404065f911e9376abbaaa48d8361e52d7
Yijia Chen <kevinc110604@gmail.com>
2023-06-20 02:17:16 -0400

    [fix] Edit post() resolver's findOne

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -138,9 +138,9 @@ export class PostResolver {
 
   @Query(() => Post, {nullable: true}) 
   post(
-    @Arg('id') id: number,
+    @Arg('id', () => Int) id: number,
   ): Promise<Post|null> {
-    return Post.findOneBy({id});
+    return Post.findOne({where: {id}, relations: {creator: true}});
   }
 
   @Mutation(() => Post) 
