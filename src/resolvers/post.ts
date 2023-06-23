73dd3c623b035fb9712ae6863f4473115ed186e6
Yijia Chen <kevinc110604@gmail.com>
2023-06-22 23:46:15 -0400

    [fix] Use createQueryBuilder to update post

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -156,15 +156,17 @@ export class PostResolver {
   async updatePost(
     @Arg('id', () => Int) id: number,
     @Arg('title', () => String, {nullable: true}) title: string,
+    @Arg('text', () => String, {nullable: true}) text: string,
+    @Ctx() {req}: MyContext
   ): Promise<Post | null> {
-    const post = await Post.findOneBy({id});
-    if (!post) {
-      return null;
-    }
-    if (typeof title !== "undefined") {
-      await Post.update({id}, {title});
-    }    
-    return post;
+    const post = await AppDataSource
+      .createQueryBuilder()
+      .update(Post)
+      .set({ title, text })
+      .where('id = :id and "creatorId" = :creatorId', { id, creatorId: req.session.userId })
+      .returning("*")
+      .execute()
+    return post.raw[0];
   }
 
   @Mutation(() => Boolean) 
